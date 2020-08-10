c3b7e0a69ee6cb120ce9c42ffbc5f80f11a49345
Dimana Nikolova <dimanannikolova@gmail.com>
2020-08-10 13:31:47 +0300

    something wrong with css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
 
     return <div className="footer">
-        <div id="button"></div>
+        <div id="footerButton"></div>
         <div id="footerCont">
            
             <div id="cont">
                 <div className="footer_center">
                     <h5>Copyright © 2020 by Dimana Nikolova</h5>
-            <div className="links">
+            <div >
                 <li className="footerLink"><Link to="/partners">Partners</Link></li>
                 <li className="footerLink"><Link to="/about">About us</Link></li>
                 <li className="footerLink"><Link to="/contacts">Contacts</Link></li>
