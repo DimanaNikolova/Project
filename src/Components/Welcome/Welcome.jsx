94f19f305461171e31fe0e1868fa1170134f6b20
Dimana Nikolova <dimanannikolova@gmail.com>
2020-08-15 21:12:04 +0300

    welcome page data fix

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -7,9 +7,9 @@ const Welcome = () => {
   return <div className="container">
     <Helmet><title>ART-icles</title></Helmet>
     <div className="item">
-      <h3>Uppercase Heading</h3>
-      <h1>Main Title Here</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ornare erat eu semper tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit viverra leo in tempor.</p>
+      <h3>The art newspaper</h3>
+      <h1>ART-ICLES</h1>
+      <p>An online and print publication that covers the international art world and most importantly strives to bring together people who have common interests and love for art to create a kind community</p>
       <Link to="/login" className="btn btn-primary btn-login">Login</Link>
       <Link to="/register" className="btn btn-primary">Register</Link>
     </div>
--- src/utils/handleChange.jsx
+++
@@ -1,8 +0,0 @@
-
-const handleChange = (e, type) => {
-    const newState = {}
-    newState[type] = e.target.value
-    this.setState(newState)
-}
-
-export default handleChange
